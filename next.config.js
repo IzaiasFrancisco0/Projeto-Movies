7f998b0714c3cad54450e9abf6a8e2b41d263151
Izaias Francisco <izaiasfrancisco2020@gmail.com>
2024-12-05 09:55:24 -0300

    Deploy do projeto Next.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const isProd = process.env.NODE_ENV === 'production';
     
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         prependData: `@import "./src/styles/variables.scss";`
-    }
+    },
+    assetPrefix: isProd ? '/Projeto-Movies/' : '',
+    basePath: isProd ? '/Projeto-Movies' : '',
 }
